ada0c8070f14b195b173eccbc65920277c7f7ce3
Erson14 <93477732+Erson14@users.noreply.github.com>
2021-10-31 21:01:17 +0700

    Add files via upload

---
+++ formpesanan.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<style>
+    html{
+        font-family: Calibri;
+    }
+    table{
+        border: none;
+        border-color: #FFEBCD;
+    }
+</style>
+<html lang="en" style="background-color:#FFEBCD ;">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PESANAN</title>
+</head>
+<body>
+    <h1 style="text-align: center;font-family:Times New Roman;">THANK YOU</h1>
+    <hr style="color: blue;">
+    <p style="font-size: 25px;">Thank you for ordering from black goose Bistro. We have
+        received the following <br> information about your order:
+        </p> 
+    <h2 style="color: salmon;"><b>Your Information</b></h2>
+<table border="1" cellspacing="1" cellpadding="1" border-style="none" style="margin-left: 60px;"> 
+            <tr>
+                <td><b>Name</b></td>
+                <td> : Erson Fairman Weryadi</td>
+            </tr>
+            <tr>
+                <td><b>Address </b></td>
+                <td>  : Cikaret Harapan jaya</td>
+            </tr>
+            <tr>
+                <td><b>Telephone Number </b></td>
+                <td> : 0895 6391 61818</td>
+            </tr>         
+            <tr>
+                <td><b>Email Address </b></td>
+                <td> : ersonfairman11@gmail.com</td>
+            </tr>           
+
+</table>
+<br>
+<p style="font-size: 25px;"><b>Delivery instruction: </b> Ring the middle buzzer. if nobody answers,
+    text me.
+        </p>
+        <h2 style="color: salmon;"><b>Your Pizza</b></h2>
+        <table border="1" cellspacing="1" cellpadding="1" border-style="none" style="margin-left: 60px;"> 
+            <tr>
+                <td><b>Crust</b></td>
+                <td> : White</td>
+            </tr>
+            <tr>
+                <td><b>Toppings </b></td>
+                <td>  : red sauce, mozzarella, pepperoni, mushrooms</td>
+            </tr>
+            <tr>
+                <td><b> Number </b></td>
+                <td> : 1</td>
+            </tr>         
+          
+
+</table>
+<br>
+<hr>
+<small>This site is for educational only, No pizzas will be delivered</small>
+</body>
+</html>
